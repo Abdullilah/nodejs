bfa9197f2373bba7c14ad833812e903ce37d4ec5
abdullilaph <abdullilaphd@hotmail.com>
2018-01-31 12:28:21 +0100

    Use another file with get and sendFile 404 not Found Page

--- express.js
+++ express.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
+
 
 // access file from the server
 app.use('/myfiles', express.static(__dirname + '/files'));
 
 app.get('/', function (request, response) {
     response.sendFile('index.html', {root: path.join(__dirname + '/files')});
+});
+
+app.get(/^(.+$)/, function (req, res) {
+    try{
+        if(fs.statSync(path.join(__dirname + '/files' , req.params[0]+'.html')).isFile()){
+            res.sendFile(req.params[0]+'.html', {root: path.join(__dirname + '/files')})
+        }
+    } catch (error){
+        console.log(error);
+        res.sendFile('404page.html', {root: path.join(__dirname + '/files')})
+    }
+
 });
 
 // listen to the port
